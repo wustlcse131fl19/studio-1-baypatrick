2e83ccd4befd202adeaf4fd2b4a185b1a0d2ecb6
bayjohnson <bay.johnson@wust.edu>
2019-08-29 12:02:22 -0500

    save

--- src/studio1/HiFour.java
+++ src/studio1/HiFour.java
@@ -16,6 +16,7 @@ public class HiFour {
 		//
 		// Say hello to the names in s0 through s3.
 		//
+System.out.println("Hello. How are you doing " + s0 + ", " + s1 + ", " + s2 + ", " + s3 + "?");
 
 	}
 }
